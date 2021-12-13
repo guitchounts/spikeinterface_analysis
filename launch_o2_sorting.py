69acc25e83bc4fa547a17159fbca7076c7d8f360
guitchounts <g.guitchounts@gmail.com>
2021-12-13 15:16:30 -0500

    sorting pipeline changes

--- launch_o2_sorting.py
+++ launch_o2_sorting.py
@@ -12,15 +12,26 @@ settings = parser.parse_args();
 param_fn = "params_waveform_save_%s.txt" % settings.expt
 fo = open(param_fn, "w")
 
-os.makedirs("/n/groups/datta/guitchounts/ephys_results/spike_sorting_results/%s/data" % settings.expt)
-os.makedirs("/n/groups/datta/guitchounts/ephys_results/spike_sorting_results/%s/logs" % settings.expt) # /n/home13/asaxe/gendynamics/results/randrelu
+
+data_path = "/n/groups/datta/guitchounts/ephys_results/spike_sorting_results/%s/data" % settings.expt
+logs_path = "/n/groups/datta/guitchounts/ephys_results/spike_sorting_results/%s/logs" % settings.expt
+
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+if not os.path.exists(logs_path):
+    os.makedirs(logs_path)
 
 expt = settings.expt
 
 
 
-mice = ['gmou03','gmou04','gmou05']
-
+#mice = ['gmou03','gmou04','gmou05']
+#mice = ['gmou11','gmou12']
+#mice = ['gmou15']
+#mice = ['gmou27']
+#mice = ['gmou31','gmou32']
+#mice = ['gmou31']
+mice = ['gmou32']
 ###
 
 
@@ -30,7 +41,16 @@ for mouse in mice:
 
     data_path = '/n/groups/datta/guitchounts/data/%s/' % mouse
     
-    all_mouse_files = [fil for fil in os.listdir(data_path) if (os.path.isdir(data_path + fil) and fil.startswith('2020') ) ] 
+    # take only folders within each mouse's dir that 1) start w/ 2020- and 2) have an 'experiment1' folder/file inside (that's where ephys data is). 
+    #all_mouse_files = [fil for fil in os.listdir(data_path) if (os.path.isdir(data_path + fil) and fil.startswith('2020') and 'experiment1' in os.listdir(data_path + fil)  ) ] 
+    #all_mouse_files = [fil for fil in os.listdir(data_path) if (os.path.isdir(data_path + fil) and mouse in fil ) ] 
+    #all_mouse_files = ['gmou15_2021-07-26_17-11-53_odor']
+    #all_mouse_files = ['gmou27_2021-08-26_10-10-19_odor']
+    #all_mouse_files = ['gmou15_2021-08-20_10-58-01_odor','gmou15_2021-09-07_11-29-58_odor','gmou15_2021-04-07_11-05-36','gmou15_2021-08-31_11-07-55_odor','gmou15_2021-04-14_13-07-11','gmou15_2021-08-27_11-07-41_odor','gmou15_2021-04-02_13-31-50','gmou15_2021-04-13_14-30-18','gmou15_2021-08-20_15-30-30_odor','gmou15_2021-08-19_14-57-13_odor','gmou15_2021-07-26_13-22-20','gmou15_2021-04-12_14-36-34','gmou15_2021-04-01_12-38-44','gmou15_2021-08-30_12-02-20_odor','gmou15_2021-04-09_14-36-53','gmou15_2021-09-03_11-00-04_odor','gmou15_2021-09-02_09-33-07_odor','gmou15_2021-04-02_16-19-01','gmou15_2021-08-17_16-55-55_wheeltest','gmou15_2021-03-31_11-49-34','gmou15_2021-03-30_14-43-31','gmou15_2021-08-26_11-22-38_odor','gmou15_2021-04-08_12-06-19']
+
+    all_mouse_files = [fil for fil in os.listdir(data_path) if (os.path.isdir(data_path + fil)   ) ] 
+    #all_mouse_files = ['gmou31_2021-10-05_13-55-04_bucket', 'gmou31_2021-10-06_14-04-04_odor', 'gmou31_2021-10-07_12-24-05_bucket',
+  #'gmou31_2021-10-05_11-51-37_odor', 'gmou31_2021-10-06_12-18-52_bucket', 'gmou31_2021-10-07_10-43-22_odor']
 
     for fil in all_mouse_files:
 
@@ -45,4 +65,4 @@ for mouse in mice:
                     
 fo.close()
 
-call("python run_o2_array.py -cmd run_sorting.py -expt %s -cores 8 -hours 6 -mem 64000 -partition short -paramfile %s" % (expt,param_fn), shell=True)
+call("python run_o2_array.py -cmd run_sorting.py -expt %s -cores 8 -hours 6 -mem 32000 -partition short -paramfile %s" % (expt,param_fn), shell=True)
--- run_sorting.py
+++ run_sorting.py
@@ -4,17 +4,19 @@ import sys,os
 import pylab
 from scipy import stats,signal,io
 import matplotlib.pyplot as plt
-from sklearn import linear_model
+
 import pickle
 import seaborn as sns
 sns.set_style('white')
 sns.set_style('ticks')
 import h5py
-import matplotlib.gridspec as gridspec
+
+
+import matplotlib
+matplotlib.use('pdf')
+
 plt.rcParams['pdf.fonttype'] = 'truetype'
-from sklearn.linear_model import LogisticRegression,SGDClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn import metrics
+
 from functools import reduce
 import datetime
 
@@ -27,7 +29,9 @@ import spikeinterface.widgets as sw
 
 import argparse
 from subprocess import call
+import time
 
+from glob import glob
 
 parser = argparse.ArgumentParser()
 
@@ -60,64 +64,126 @@ print('Running Spike Interface script on %s path... Path exists==%s' % (base_pat
 
 
 
-data = se.OpenEphysRecordingExtractor(base_path)
 
-recording = data.load_probe_file(probe_file='%s/../../32_groups.prb' % base_path)
+data = se.OpenEphysRecordingExtractor(glob('%s/Record*' % base_path)[0])
+
+#recording = data.load_probe_file('/home/gg121/code/spikeinterface_analysis/64ch_cnt_probe_mapped.prb')
+
+probe_file = glob('%s/../*.prb' % base_path)[0]  #'/home/gg121/code/spikeinterface_analysis/A4x16-Poly3-5mm-20-200-160-H64LP.prb'
+
+recording = data.load_probe_file(probe_file)
+
+#recording = data.load_probe_file(probe_file='/n/groups/datta/guitchounts/data/64ch_cnt_probe_shanks.prb') # '%s/../../32_groups.prb' % base_path
 
 print('Channel ids:', recording.get_channel_ids())
 print('Loaded properties', recording.get_shared_channel_property_names())
-print('Label of channel 0:', recording.get_channel_property(channel_id=0, property_name='label'))
+#print('Label of channel 0:', recording.get_channel_property(channel_id=0, property_name='label'))
+
 
 recording_f = st.preprocessing.bandpass_filter(recording, freq_min=300, freq_max=6000)
 recording_cmr = st.preprocessing.common_reference(recording_f, reference='median')
 
 
+num_workers = 8
+
+
 default_ms4_params = ss.Mountainsort4Sorter.default_params()
 #print(default_ms4_params)
 ms4_params = default_ms4_params.copy()
-ms4_params['adjacency_radius'] = 0
+ms4_params['adjacency_radius'] = 50
 ms4_params['detect_sign'] = 0
 ms4_params['filter'] = False
-ms4_params['num_workers'] = 8
+ms4_params['num_workers'] = num_workers
 fs = 3e4
 
 
-default_kl_params = ss.KlustaSorter.default_params()
-#print(default_kl_params)
-kl_params = default_kl_params.copy()
-kl_params['adjacency_radius'] = 0
-kl_params['extract_s_before'] = 32
-kl_params['detect_sign'] = 0
-kl_params['filter'] = False
-kl_params['num_workers'] = 8
+
+
+# default_kl_params = ss.KlustaSorter.default_params()
+# #print(default_kl_params)
+# kl_params = default_kl_params.copy()
+# kl_params['adjacency_radius'] = 50
+# kl_params['extract_s_before'] = 32
+# kl_params['detect_sign'] = 0
+# #kl_params['filter'] = False
+# #kl_params['num_workers'] = num_workers
+
+
+
 
 
 #### run sorting:
-
+print('Starting mountainsort4 sorting....')
+start = time.time()
 # run Mountainsort:
-sorting_MS4 = ss.run_sorter('mountainsort4',recording_cmr,grouping_property='group',
+sorting_MS4 = ss.run_sorter('mountainsort4',recording_cmr, #grouping_property='group',
+                       parallel=True,
                        output_folder='%s/tmp_MS4' % base_path, **ms4_params)
-
-sorting_MS4 = st.curation.threshold_firing_rates(sorting_MS4, 
-    duration_in_frames=recording_cmr.get_num_frames(), threshold=0.05, threshold_sign='less')
-
-sorting_MS4 = st.curation.threshold_snrs(sorting_MS4, recording_cmr, threshold=5, threshold_sign='less')
-
-st.postprocessing.export_to_phy(recording_cmr, sorting_MS4, output_folder='%s/phy_MS4' % base_path, grouping_property='group')
+print('sorting finished in %f seconds...' % (time.time() - start) )
 
 
+#sorting_MS4 = st.curation.threshold_firing_rates(sorting_MS4, 
+#    duration_in_frames=recording_cmr.get_num_frames(), threshold=0.05, threshold_sign='less')
+
+#sorting_MS4 = st.curation.threshold_snrs(sorting_MS4, recording_cmr, threshold=5, threshold_sign='less')
+
+#st.postprocessing.export_to_phy(recording_cmr, sorting_MS4, output_folder='%s/phy_MS4' % base_path, grouping_property='group')
+
+num_frames = recording_cmr.get_num_frames()
+
+all_unit_ids = sorting_MS4.get_unit_ids()
+
+# high_snr_units =  np.where(st.validation.compute_snrs(sorting_MS4, recording_cmr) > 3.5)[0] 
+# isi_units = np.where(st.validation.compute_isi_violations(sorting_MS4, num_frames) < 1)[0]
+
+metrics = st.validation.compute_quality_metrics(sorting=sorting_MS4, recording=recording_cmr,
+                                                metric_names=['firing_rate', 'isi_violation', 'snr', 'nn_hit_rate', 'nn_miss_rate'],
+                                                as_dataframe=True)
 
 
-# run KlustaKwik:
-sorting_KL = ss.run_sorter('klusta',recording_cmr,grouping_property='group',
-                       output_folder='%s/tmp_KL' % base_path, **kl_params)
+#good_units = np.intersect1d(high_snr_units,isi_units) # for plotting 
+good_units = np.intersect1d(np.where(metrics['isi_violation'].values <= 1.5), np.where(metrics['snr'].values >=3.5 ) )
 
-sorting_KL = st.curation.threshold_firing_rates(sorting_KL, 
-    duration_in_frames=recording_cmr.get_num_frames(), threshold=0.05, threshold_sign='less')
+np.savez('%s/tmp_MS4/unit_properties' % base_path,all_unit_ids=all_unit_ids,metrics=metrics,good_units=good_units)
 
-sorting_KL = st.curation.threshold_snrs(sorting_KL, recording_cmr, threshold=5, threshold_sign='less')
+print('saved unit properties')
 
-st.postprocessing.export_to_phy(recording_cmr, sorting_KL, output_folder='%s/phy_KL' % base_path, grouping_property='group')
+sorting_MS4.dump_to_pickle('%s/tmp_MS4/sorting.pickle' % base_path)
+print('saved unit sorting pickle')
+
+waveform_params = st.postprocessing.get_waveforms_params()
+waveform_params['ms_before'] = 1
+waveform_params['ms_after'] = 1
+#waveform_params['max_channels_per_waveforms'] = 1 
+waveform_params['n_jobs'] = 8
+waveform_params['max_spikes_per_unit'] = 1000
+
+
+
+waveforms = st.postprocessing.get_unit_waveforms(recording_cmr,sorting_MS4, return_idxs=False,
+                                                 **waveform_params
+                                                )
+
+
+
+np.savez('%s/tmp_MS4/waveforms' % base_path,{item : waveforms[item] for item in range(len(waveforms))})
+
+
+print('saved waveforms')
+
+
+# print('Starting klusta sorting....')
+# # run KlustaKwik:
+# sorting_KL = ss.run_sorter('klusta',recording_cmr, #grouping_property='group',
+#                     parallel=True,
+#                        output_folder='%s/tmp_KL' % base_path, **kl_params)
+
+# # sorting_KL = st.curation.threshold_firing_rates(sorting_KL, 
+# #     duration_in_frames=recording_cmr.get_num_frames(), threshold=0.05, threshold_sign='less')
+
+# # sorting_KL = st.curation.threshold_snrs(sorting_KL, recording_cmr, threshold=5, threshold_sign='less')
+
+# # st.postprocessing.export_to_phy(recording_cmr, sorting_KL, output_folder='%s/phy_KL' % base_path, grouping_property='group')
 
 
 
@@ -125,27 +191,56 @@ st.postprocessing.export_to_phy(recording_cmr, sorting_KL, output_folder='%s/phy
 
 
 
-print('Number of KlustaKwik units after curation:', len(sorting_KL.get_unit_ids()))
-print('Number of Mountainsort units after curation:', len(sorting_MS4.get_unit_ids()))
+
+# print('Number of KlustaKwik units after curation:', len(sorting_KL.get_unit_ids()))
+# #print('Number of Mountainsort units after curation:', len(sorting_MS4.get_unit_ids()))
 
 
 
 
 ## plot 
-f = plt.figure(dpi=600)
-ax = plt.subplot(111)
-sw.plot_unit_waveforms(recording_cmr,sorting,ax=ax,unit_ids=sorting_MS4.get_unit_ids(),
-                      max_channels=1, ms_after=1,
-                              )
-f.savefig('%s/tmp_MS4/waveforms.pdf' % base_path)
+#f = plt.figure(dpi=600)
+# ax = plt.subplot(111)
+# sw.plot_unit_waveforms(recording_cmr,sorting_MS4,ax=ax,unit_ids=sorting_MS4.get_unit_ids(),
+#                       max_channels=1, ms_after=1,
+#                               )
+
+### get mapping of channel id to location:
+channel_ids = recording.get_channel_ids()
+channel_locations = recording.get_channel_locations()
+data_channels = {}
+for i in range(64):
+    data_channels[channel_ids[i]] = channel_locations[i]
 
 
-## plot 
-f = plt.figure(dpi=600)
-ax = plt.subplot(111)
-sw.plot_unit_waveforms(recording_cmr,sorting,ax=ax,unit_ids=sorting_KL.get_unit_ids(),
-                      max_channels=1, ms_after=1,
-                              )
-f.savefig('%s/tmp_KL/waveforms.pdf' % base_path)
+for unit in good_units:
+
+    f = plt.figure(dpi=600,figsize=(4,4))
+    for ch,key in enumerate(data_channels):
+    #for key in range(64):
+        plt.plot(np.arange(0,20,20/60) + data_channels[key][0],
+                waveforms[unit][:, ch, 60:120].mean(axis=0) + data_channels[key][1], 
+                color='k', lw=0.25)
+
+    plt.title('unit %d, snr=%.2f, ISI=%.2f' % (unit,metrics['snr'].values[unit],
+                                               metrics['isi_violation'].values[unit] ) )
+    sns.despine(left=True,bottom=True)
+    
+    f.savefig('%s/tmp_MS4/unit_%d.pdf' % (base_path,unit) )
+    
+    plt.close(f)
+
+
+#f.savefig('%s/tmp_MS4/waveforms.pdf' % base_path)
+
+print('plotting waveforms')
+
+# ## plot 
+# f = plt.figure(dpi=600)
+# ax = plt.subplot(111)
+# sw.plot_unit_waveforms(recording_cmr,sorting_KL,ax=ax,unit_ids=sorting_KL.get_unit_ids(),
+#                       max_channels=1, ms_after=1,
+#                               )
+# f.savefig('%s/tmp_KL/waveforms.pdf' % base_path)
 
 
